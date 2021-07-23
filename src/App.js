a435c515a4a929ff2da1fda68f41a0bc323933a6
ominukamauro <ominuka.mauro@gmail.com>
2021-07-23 13:39:48 -0300

    Elimine el componente Signup de la pagina, para cerrar las inscripciones al evento.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Navbar from './components/Navbar'
 import Contact from './components/Contact'
 import About from './components/About'
 import Projects from './components/Projects'
-import Signup from './components/Signup'
+/* import Signup from './components/Signup'
+ */
 import Footer from './components/Footer'
 import Header from './components/Header'
 import { SnackbarProvider } from 'snackfy'
@@ -17,7 +18,17 @@ const App = () => {
           <Header />
           <About />
           <Projects />
-          <Signup />
+          <div>
+            <section className='signup-section' id='signup'>
+              <div className='container'>
+                <div className='row'>
+                  <div className='col-md-10 col-lg-8 mx-auto text-center'>
+                    <h2 className='text-white mb-1'>Las inscripciones para el encuentro del 24 y 25 de Julio se encuentran cerradas. </h2>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
           <Contact />
           <Footer />
         </main>
